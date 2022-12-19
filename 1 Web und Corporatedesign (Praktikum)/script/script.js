650b86a4c24d5cb094a79bccff974b8af46686d1
Pavel Git <pavel.git@web.de>
2022-12-19 23:20:43 +0100

    Registrieren/Anmelden update

--- 1 Web und Corporatedesign (Praktikum)/script/script.js
+++ 1 Web und Corporatedesign (Praktikum)/script/script.js
@@ -1,7 +1,28 @@
-var profil = document.getElementsByClassName("nav11")[0];
-profil.addEventListener("click", logReg());
+var passwordOkM;
+var emailOk;
+var emailFormat = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{8,}$/;
+var passwordForamt = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-function logReg (){
-    location.href = ""
-}
+checkLogin.addEventListener(oninput, function(){
+    if (checkLogin.length > 0) {
+        loginOk = true;
+    }
+
+    var password = document.getElementsByClassName("password")[0].value.trim();
+    if (password.length > 0 && password.search(passwordForamt) > -1  ) {
+        passwordOk = true;
+    } 
+
+    var email = document.getElementsByClassName("email")[0].value.trim();
+    if (email.length > 0 && email.search(emailFormat) > -1) {
+        emailOK = true;
+    }
+
+    if (!passwordOK && !emailOk) {
+
+        document.getElementsByClassName("login")[0].style="background-color: #ff7f7f";
+    }
+
+});
+
     
